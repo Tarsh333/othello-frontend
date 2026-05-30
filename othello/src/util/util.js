2e61c2d8c79d261989702c5765f43066c28cfde0
parna-banerjee <parnab2000@gmail.com>
2026-05-30 14:19:34 +0530

    initial board 2x2

--- othello/src/util/util.js
+++ othello/src/util/util.js
@@ -3,11 +3,22 @@ import { ROWS,COLS ,CellState} from "../constants/Constants";
 
 export function initializeBoard() {
 
-  return Array.from({ length: ROWS }, () =>
+  const board = Array.from({ length: ROWS }, () =>
     Array.from({ length: COLS }, () => ({
       color: CellState.EMPTY
     }))
   );
+
+  const midRow = ROWS / 2;
+  const midCol = COLS / 2;
+
+  board[midRow - 1][midCol - 1].color = CellState.WHITE;
+  board[midRow][midCol].color = CellState.WHITE;
+
+  board[midRow - 1][midCol].color = CellState.BLACK;
+  board[midRow][midCol - 1].color = CellState.BLACK;
+
+  return board;
 }
 
 export function getColorForCell(cell) {
